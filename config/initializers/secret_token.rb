e11fb2020837ab9fae446f0d6f26d616176cec4c
hari <hariswaroop.gangwar@gmail.com>
2014-01-31 04:31:11 +0530

    refractoring

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '743337ed2c1add95c461d87e1424c644d8460b40dca479cd2effc3f7289b7b1819d903b3bd052c6583f081b04057dd8f097ffbbd1b434be32e1c625637ac7069'
