5219ac4928c81c73e3d267691196a14fb4b259dd
Hariswaroop <hariswaroop.gangwar@gmail.com>
2014-05-25 14:28:05 +0530

    ready for development mode

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -7,4 +7,5 @@ ActionMailer::Base.smtp_settings= {
 	authentication: "plain",
 	enable_starttls_auto: true
 } 
-	Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.Development?
+	ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+	Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
