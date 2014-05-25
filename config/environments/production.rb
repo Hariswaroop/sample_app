96a49f032ce59b98e1bfefc20106a223387aa141
Hariswaroop <hariswaroop.gangwar@gmail.com>
2014-05-26 00:53:30 +0530

    mailing function in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,11 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.delivery_method = :smtp,
+ 
+  config.action_mailer.default_url_options = {:host => 'make-post.herokuapp.com', :protocol => 'http'}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
