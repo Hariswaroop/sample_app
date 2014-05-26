78bcb4ebfa0cc8109d1ae71d184f1cff233f67e6
Hariswaroop <hariswaroop.gangwar@gmail.com>
2014-05-27 03:00:49 +0530

    field stste removed from users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140306133700) do
+ActiveRecord::Schema.define(version: 20140526212643) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
